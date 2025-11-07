f3b538f33068c701305dc0a2d2e6765d9ce8564f
Luke44565 <luke.j.levine@gmail.com>
2025-11-06 21:25:30 -0500

    add map and collision

--- Tino.gd
+++ Tino.gd
@@ -1,18 +1,16 @@
 extends CharacterBody2D
 
-var screen_size
+const speed = 10;
 
 func _ready():
-	screen_size = get_viewport_rect().size
+	pass
 	
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_pressed(&"move_right"):
-		position += Vector2(10, 0)
+		move_and_collide(Vector2(speed, 0))
 	if Input.is_action_pressed(&"move_left"):
-		position += Vector2(-10, 0)
+		move_and_collide(Vector2(-speed, 0))
 	if Input.is_action_pressed(&"move_down"):
-		position += Vector2(0, 10)
+		move_and_collide(Vector2(0, speed))
 	if Input.is_action_pressed(&"move_up"):
-		position += Vector2(0, -10)
-	
-	position = position.clamp(Vector2.ZERO, screen_size)
+		move_and_collide(Vector2(0, -speed))
